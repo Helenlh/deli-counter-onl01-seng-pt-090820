11ac23740e90287547f6cf79baa8a1877c6b57d2
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-20 19:40:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,63 @@
-def deli_counter(name)
-  if name == []
+
+
+  # let(:katz_deli) { [] }
+  # let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+  # let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", #"Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
+
+# def deli_counter(name)
+#   if name == []
+#     puts "The line is currently empty."
+#     else 
+#       name.each_with_index do |name|
+#         puts "The line is currently #{name}"
+# end 
+# end 
+# end 
+
+
+
+
+def line(name)
+ if name == []
     puts "The line is currently empty."
+
   else 
-  names.each_with_index {|name| 
-  puts "The line is currently #{name}"}
+    name.each_with_index do |names|
+    puts "The line is currently #{names}"
+    
 end 
 end 
-
-def line(name)
-
-
-end 
-
-def take_a_number(names, new_name)
-  if new_name.length > 0 
-    puts "Welcome, #{new_name}.  You are number (new_name.length) in line"
-    names.push = currently_serving
-    puts ("The line is currently" + counter + names)
-    counter +=1 
-  end 
-end 
+end
 
 
 
-def now_serving
-  if names == []
-    puts "The line is currently empty."
-  elsif puts "currently serving #{names}"
-  names.push
-end 
-end 
+# def take_a_number(names, new_name)
+#       if names == []
+#     puts "The line is currently empty."
+#     names << new_name
+   
+#   elsif  
+#       names << new_name
+#       names.each_with_index do |mew_name|  
+#       names << new_name
+#       puts "Welcome, #{new_name}. You are number #{new_name.length} in line."
+#       names.each do |names|
+#       puts "The line is currently #{names}."
+#         end 
+# end 
+# end 
+#end 
+
+
+
+
+
+# def now_serving(names)
+#   if names == []
+#     puts "There is nobody waiting to be served!"
+#   elsif names.each_with_index do |names| 
+#     puts "Currently serving #{names}."
+#   names.shift
+# end 
+# end 
+# end 
