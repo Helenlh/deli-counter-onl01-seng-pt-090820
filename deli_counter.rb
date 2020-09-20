3595e3fc327ec88b62cdcab7d47948b85a05c2fe
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-20 22:20:16 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,15 @@
   # let(:other_deli) { ["Logan", "Avi", "Spencer"] }
   # let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", #"Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
 
-# def deli_counter(name)
-#   if name == []
-#     puts "The line is currently empty."
-#     else 
-#       name.each_with_index do |name|
-#         puts "The line is currently #{name}"
-# end 
-# end 
-# end 
+def deli_counter(name)
+  if name == []
+    puts "The line is currently empty."
+    else 
+      name.each_with_index do |name|
+        puts "The line is currently #{name}"
+end 
+end 
+end 
 
 
 
@@ -22,42 +22,42 @@ def line(name)
     puts "The line is currently empty."
 
   else 
-    name.each_with_index do |names|
-    puts "The line is currently #{names}"
+    name.each_with_index do |name|
+    end 
+    puts "The line is currently #{name}"
     
-end 
 end 
 end
 
 
 
-# def take_a_number(names, new_name)
-#       if names == []
-#     puts "The line is currently empty."
-#     names << new_name
+def take_a_number(names, new_name)
+      if names == []
+    puts "The line is currently empty."
+    names << new_name
    
-#   elsif  
-#       names << new_name
-#       names.each_with_index do |mew_name|  
-#       names << new_name
-#       puts "Welcome, #{new_name}. You are number #{new_name.length} in line."
-#       names.each do |names|
-#       puts "The line is currently #{names}."
-#         end 
-# end 
-# end 
-#end 
+  elsif  
+      names << new_name
+      names.each_with_index do |mew_name|  
+      names << new_name
+      puts "Welcome, #{new_name}. You are number #{new_name.length} in line."
+      names.each do |names|
+      puts "The line is currently #{names}."
+        end 
+end 
+end 
+end 
 
 
 
 
 
-# def now_serving(names)
-#   if names == []
-#     puts "There is nobody waiting to be served!"
-#   elsif names.each_with_index do |names| 
-#     puts "Currently serving #{names}."
-#   names.shift
-# end 
-# end 
-# end 
+def now_serving(names)
+  if names == []
+    puts "There is nobody waiting to be served!"
+  elsif names.each_with_index do |names| 
+    puts "Currently serving #{names}."
+  names.shift
+end 
+end 
+end 
