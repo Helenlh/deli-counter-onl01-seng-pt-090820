1df9ffa5fe233c34da68fbdc74b23824679da11a
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-21 17:21:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def line(name)
  if name == []
     puts "The line is currently empty." 
     else 
-      while names > 0 
+      while name > 0 
 name.collect do|names|
       puts "The line is currently #{names}"
     end 
